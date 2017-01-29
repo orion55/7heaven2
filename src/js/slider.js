fefdf498112e7578ae03f1be9f256e0c27897f2a
orion55 <grol55@mail.ru>
2017-01-29 19:40:11 +0500

    first commit

---
+++ src/js/slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#slider-main').bxSlider(
+        {
+            preloadImages: true,
+            mode: 'fade',
+            controls: false,
+            auto: true,
+            autoStart: true,
+            pause: 5000,
+            pager: true
+        }
+    );
+});
